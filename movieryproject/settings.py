90d6863214e1781bd738b836d469e9ccac012bcb
Elisson Douglas <97467970+ElissonDouglas@users.noreply.github.com>
2023-02-14 12:08:28 -0300

    .

--- movieryproject/settings.py
+++ movieryproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-x496&wq98a)369nsvo+=h7$c*n)@esrl9+(fs$z+lxxd8n2#ry'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
